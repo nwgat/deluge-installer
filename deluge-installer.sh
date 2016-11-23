32a2ee83fa93012894fee93ecee89cb7285e7c78
nwgat <github@nwgat.net>
2016-11-24 00:15:12 +0100

    Update deluge-installer.sh

--- deluge-installer.sh
+++ deluge-installer.sh
@@ -12,15 +12,16 @@ read -e pw
 
 echo ""
 echo "Installing Packages"
-apt-get install -qq software-properties-common -y >> /dev/null
-add-apt-repository ppa:deluge-team/ppa -y >> /dev/null
-apt-get update -qq >> /dev/null
-apt-get install -qq deluged deluge-web deluge-console nano wget python-pip -y >> /dev/null
-pip install -q flexget >> /dev/null
+apt-get install -qq software-properties-common -y
+add-apt-repository ppa:deluge-team/ppa -y
+apt-get update -qq
+apt-get install -qq deluged deluge-web deluge-console nano wget python-pip ufw -y
+pip install -q flexget
 
 echo "Setting up Systemd"
-wget -q https://raw.githubusercontent.com/nwgat/etc/master/deluge/deluged.service -O /etc/systemd/system/deluged.service
-wget -q https://raw.githubusercontent.com/nwgat/etc/master/deluge/deluge-web.service -O /etc/systemd/system/deluge-web.service
+wget https://raw.githubusercontent.com/nwgat/etc/master/deluge/deluged.service -O /etc/systemd/system/deluged.service
+wget https://raw.githubusercontent.com/nwgat/etc/master/deluge/deluge-web.service -O /etc/systemd/system/deluge-web.service
+systemctl daemon-reload
 
 echo "Opening Firewall"
 ufw allow 8122
@@ -30,18 +31,19 @@ ufw allow 9000
 echo "Setting up Deluge"
 adduser --disabled-password --gecos "" deluge
 
-su -c 'deluged && pkill -9 deluged' deluge
-su -c 'echo "$usr:$pw:10" >> $HOME/.config/deluge/auth' deluge
+su -c 'deluged' deluge
+su -c 'pkill -9 deluged' deluge
+echo '$usr:$pw:10' >> /home/deluge/.config/deluge/auth
+chown deluge /home/deluge/.config/deluge/auth
+systemctl start deluged deluge-web && systemctl enable deluged deluge-web
+
 su -c 'deluge-console "config -s allow_remote True"' deluge
-su -c 'deluge-console "config allow_remote"' deluge
 su -c 'deluge-console "config -s download_location /home/deluge/incomplete"' deluge
 su -c 'deluge-console "config -s move_completed_path /home/deluge/complete"' deluge
 su -c 'deluge-console "config -s listen_ports (9000, 9000)"' deluge
-su -c 'deluge-console "config -s move_completed true' deluge
+su -c 'deluge-console "config -s move_completed true"' deluge
 
-systemctl start deluged && systemctl enable deluged >> /dev/null
-systemctl start deluge-web && systemctl enable deluge-web >> /dev/null
-systemctl status deluged deluge-web >> /dev/null
+systemctl stop deluged deluge-web && systemctl start deluged deluge-web
 
 echo ""
 echo "Deluge is now setup"

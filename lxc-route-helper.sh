50956832564eefbf678757a3cdd454fc642dd821
nwgat <github@nwgat.net>
2016-11-24 16:45:41 +0100

    Create lxc-route-helper.sh

---
+++ lxc-route-helper.sh
@@ -0,0 +1,9 @@
+ip=`(hostname -I | sed 's/ //g')`
+echo "lxc container?"
+read -p lxc
+
+iptables -t nat -A PREROUTING -p tcp -d $ip --dport 58846 -j DNAT --to-destination $lxc:58846
+iptables -t nat -A PREROUTING -p tcp -d $ip --dport 8112 -j DNAT --to-destination $lxc:8112
+iptables -t nat -A PREROUTING -p tcp -d $ip --dport 9000 -j DNAT --to-destination $lxc:9000
+
+iptables -t nat -A POSTROUTING -j MASQUERADE
